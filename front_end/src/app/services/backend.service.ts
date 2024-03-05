74f35b3b8a98ea9a418114b45334a1e540a6b828
sod2003 <sod2003@gmail.com>
2024-03-05 15:54:06 -0500

    Demonstrating front-end routing.

--- front_end/src/app/services/backend.service.ts
+++ front_end/src/app/services/backend.service.ts
@@ -6,17 +6,33 @@ import { Injectable } from '@angular/core';
 })
 export class BackendService {
 
+  localHost: string = 'http://localhost:8080';
+
   constructor(private http: HttpClient) {
     this.getAllCustomers();
   }
 
   getAllCustomers() {
-    this.http.get<any>("http://localhost:8080/customers", { observe: 'response' })
+    this.http.get<any>(this.localHost + "/customers", { observe: 'response' })
     .subscribe(data => {
       // for (let customer of data.body) {
       //   console.log(customer);
       // }
       console.log(data);
     });
+  }
+
+  getCustomer(id: string) {
+    this.http.get<any>(this.localHost + "/customers/" + id, { observe: 'response'})
+    .subscribe(data => {
+      console.log(data);
+    })
+  }
+
+  getDevices() {
+    this.http.get<any>(this.localHost + "/devices", { observe: 'response'})
+    .subscribe(data => {
+      console.log(data);
+    })
   }
 }
