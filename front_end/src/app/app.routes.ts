65678a4a7124793333e8e32728cfc7f37d7cc387
Samuel Park <parksamuelsoung@gmail.com>
2024-03-05 16:28:49 -0600

    Merging conflicts

--- front_end/src/app/app.routes.ts
+++ front_end/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-// import { TestComponent } from './test/test.component';
+import { TestComponent } from './test/test.component';
 import { TestDeviceComponent } from './test/testdevice/testdevice.component';
 
 export const routes: Routes = [
-    // {
-    //     path: 'test',
-    //     component: TestComponent
-    // },
+    {
+        path: 'test',
+        component: TestComponent
+    },
     {
         path: 'testdevice',
         component: TestDeviceComponent
