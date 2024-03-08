82acead3adc4ea7bdf3173f5c1dafb66302f89d6
Samuel Park <parksamuelsoung@gmail.com>
2024-03-08 13:31:58 -0600

    Phone Plan

--- src/main/java/com/skillstorm/spartanwireless/controllers/PhonePlanController.java
+++ src/main/java/com/skillstorm/spartanwireless/controllers/PhonePlanController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skillstorm.spartanwireless.dtos.PhonePlanResponseDto;
 import com.skillstorm.spartanwireless.services.PhonePlanService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/customers/{custId}/phone_plans")
 public class PhonePlanController {
