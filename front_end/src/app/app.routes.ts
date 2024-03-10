908c4bbd242224a903fa78597c36ca78a51cb166
Samuel Park <parksamuelsoung@gmail.com>
2024-03-10 15:46:59 -0500

    Merge App Route

--- front_end/src/app/app.routes.ts
+++ front_end/src/app/app.routes.ts
@@ -2,34 +2,22 @@ import { Routes } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { TestDeviceComponent } from './test/testdevice/testdevice.component';
 import { TestphonelineComponent } from './test/testphoneline/testphoneline.component';
-<<<<<<< HEAD
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
     },
-
-    // {
-    //     path: 'devices',
-    //     component: DevicesComponent
-    // },
-=======
-import { DevicesComponent } from './components/devices/devices.component';
-import { PhonePlansComponent } from './components/phone-plans/phone-plans.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
-export const routes: Routes = [
     {
         path: 'customers/:custId',
         component: DashboardComponent
     }
     
     // ,
->>>>>>> 82d0e70 (Created PhonePlans Get All List component)
     // {
     //     path: 'test',
     //     component: TestComponent
