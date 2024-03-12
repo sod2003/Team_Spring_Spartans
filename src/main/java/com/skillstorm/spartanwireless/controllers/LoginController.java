718387e99bd568716d8aef65ce96c7e87ceea9c4
Samuel Park <parksamuelsoung@gmail.com>
2024-03-11 21:48:51 -0500

    configured login with auth manager, details service, details

--- src/main/java/com/skillstorm/spartanwireless/controllers/LoginController.java
+++ src/main/java/com/skillstorm/spartanwireless/controllers/LoginController.java
@@ -1,6 +1,11 @@
 package com.skillstorm.spartanwireless.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,9 +26,10 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
-        Authentication authenticationRequest = UsernamePasswordAuthenticationToken
-                .unauthenticated(loginRequestDto.getUsername(), loginRequestDto.getPassword());
-        Authentication authenticationResponse = this.authenticationManager.authenticate(authenticationRequest);
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                loginRequestDto.getUsername(),
+                loginRequestDto.getPassword()));
         Long custId = userRepository.findById(loginRequestDto.getUsername()).get().getCustomer().getCustId();
         return new ResponseEntity<>(new LoginResponseDto(loginRequestDto.getUsername(), custId), HttpStatus.OK);
     }
