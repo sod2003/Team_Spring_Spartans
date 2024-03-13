2040420c001b56bbc0c96e632f92ed8906d2f496
Samuel Park <parksamuelsoung@gmail.com>
2024-03-12 20:33:34 -0500

    Working on phone line with Sean

--- front_end/src/app/components/phone-lines/phone-lines.component.ts
+++ front_end/src/app/components/phone-lines/phone-lines.component.ts
@@ -3,29 +3,40 @@ import { DeviceService } from '../../services/device.service';
 import { PhonelineService } from '../../services/phoneline.service';
 import { Device } from '../../models/device';
 import { Phoneline } from '../../models/phoneline';
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-phone-lines',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './phone-lines.component.html',
   styleUrl: './phone-lines.component.css'
 })
 export class PhoneLinesComponent implements OnInit {
 
   @Input() custId: number = 0;
-  devices: Device[] = [];
+  devicesOfCustRaw: Device[] = [];
+  devicesOfCustSubject = new BehaviorSubject<Device[]>([]);
+  devicesOfCustObservable = this.devicesOfCustSubject.asObservable();
   phonelines: Phoneline[] = [];
 
-  constructor(private deviceService: DeviceService, private phonelineService: PhonelineService) {}
+  constructor(
+    private deviceService: DeviceService,
+    private phonelineService: PhonelineService
+  ) { }
 
   ngOnInit(): void {
-      this.deviceService.devicesObservable.subscribe((data) => {
-        this.devices = data;
-      });
-      this.phonelineService.allPhonelines.subscribe((data) => {
-        this.phonelines = data;
-      });
+    this.getDevicesOfCust();
+    this.phonelineService.allPhonelines.subscribe((data) => {
+      this.phonelines = data;
+    });
+  }
+
+  getDevicesOfCust() {
+    for (let phoneline of this.phonelines) {
+      this.devicesOfCustRaw.push(this.deviceService.getDeviceById(phoneline.deviceId));
+    }
   }
 
   addPhoneline() {
