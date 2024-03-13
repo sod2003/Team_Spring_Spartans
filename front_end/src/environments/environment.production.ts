e2cac4309d91a41c5b3a5778a6bec8696d328562
Samuel Park <parksamuelsoung@gmail.com>
2024-03-13 13:44:25 -0500

    Bug fix

---
+++ front_end/src/environments/environment.production.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    secretKey: "secretKey123",
+    baseUrl: "http://localhost:8080",
+    loginUrl: "http://localhost:8080/login",
+    registerUrl: "http://localhost:8080/register",
+    customersUrl: "http://localhost:8080/customers"
+};
