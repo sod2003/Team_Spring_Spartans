a0bd4b449b6b76b3e1dd7e9b5bc9842a298fdd49
Samuel Park <parksamuelsoung@gmail.com>
2024-03-13 10:00:31 -0500

    stage

--- front_end/src/environments/environment.production.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    secretKey: "secretKey123",
-    baseUrl: "http://localhost:8080",
-    loginUrl: "http://localhost:8080/login",
-    registerUrl: "http://localhost:8080/register",
-    customersUrl: "http://localhost:8080/customers"
-};
