60895b71c457f165250988c7753f118c1a490d32
Samuel Park <parksamuelsoung@gmail.com>
2024-03-14 09:06:24 -0500

    Rebased

--- src/main/java/com/skillstorm/spartanwireless/controllers/PhonePlanController.java
+++ src/main/java/com/skillstorm/spartanwireless/controllers/PhonePlanController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +25,8 @@ public class PhonePlanController {
     @Autowired
     private PhonePlanService phonePlanService;
 
-    @PostMapping("/{phonePlanId}")
-    public ResponseEntity<PhonePlanResponseDto> createPhonePlan(@PathVariable Long custId, @PathVariable Long phonePlanId) {
+    @PostMapping
+    public ResponseEntity<PhonePlanResponseDto> createPhonePlan(@PathVariable Long custId, @RequestBody Long phonePlanId) {
         return new ResponseEntity<>(phonePlanService.createPhonePlan(custId, phonePlanId), HttpStatus.CREATED);
     }
 
